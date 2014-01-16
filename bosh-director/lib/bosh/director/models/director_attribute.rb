f821f09b169bfb08fe1e1c29be8cc78e2edb76e5
Rob Day-Reynolds <rdayreynolds@pivotallabs.com>
2014-01-16 09:32:21 -0800

    wip for director attribute migration
    Signed-off-by: dmitriy kalinin <dmitriy@pivotallabs.com>

--- bosh-director/lib/bosh/director/models/director_attribute.rb
+++ bosh-director/lib/bosh/director/models/director_attribute.rb
@@ -1,9 +1,20 @@
 module Bosh::Director::Models
   class DirectorAttribute < Sequel::Model(Bosh::Director::Config.db)
-
     def validate
-      validates_unique [:uuid]
+      validates_presence :name
     end
 
+    def self.find_or_create_uuid
+      uuid = first(name: 'uuid')
+      return uuid.value if uuid
+
+      begin
+        create(name: 'uuid', value: SecureRandom.uuid).value
+      rescue Sequel::DatabaseError
+        # Database will throw an error in case of race condition
+        # causing multiple uuid records being inserted
+        first(name: 'uuid').value
+      end
+    end
   end
 end
