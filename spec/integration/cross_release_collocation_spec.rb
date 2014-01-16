f26c320cb01508581c92915966a19698ec4a1572
Mike Stallard <mstallard@pivotallabs.com>
2014-01-15 17:01:19 -0800

    inline `deploy_simple_manifest`
    We tolerate for now the duplication here because this one test diverges
    from other integration tests
    
    Signed-off-by: Jesse Zhang <jz@pivotallabs.com>

--- spec/integration/cross_release_collocation_spec.rb
+++ spec/integration/cross_release_collocation_spec.rb
@@ -3,9 +3,8 @@ require 'spec_helper'
 describe 'collocating templates from 2 releases' do
   include IntegrationExampleGroup
 
-  it 'refuses to deploy when 2 templates depend on packages with the same name' do
-
-    extras = {
+  let(:extras) do
+    {
       'name' => 'simple',
       'releases' => [
         {
@@ -46,8 +45,50 @@ describe 'collocating templates from 2 releases' do
           }
         }
       ],
+    }
+  end
 
-      'jobs' => [
+  context 'when the template packages do not collide' do
+    let(:jobs) do
+      [
+        {
+          'name' => 'foobar',
+          'templates' => [
+            {
+              'name' => 'dummy_with_package',
+              'release' => 'dummy',
+            },
+            {
+              'name' => 'dummy',
+              'release' => 'dummy2',
+            },
+          ],
+
+          'resource_pool' => 'a',
+          'instances' => 1,
+          'networks' => [{'name' => 'a'}]
+        }
+      ]
+    end
+
+    it 'successfully deploys' do
+      base_manifest = make_collocated_manifest
+      manifest_hash = base_manifest.merge(extras).merge('jobs' => jobs)
+
+      upload_stemcell_and_releases
+
+      deployment_manifest = yaml_file('simple', manifest_hash)
+      run_bosh("deployment #{deployment_manifest.path}")
+
+      output = run_bosh("deploy")
+      expect($?).to be_success
+      p output
+    end
+  end
+
+  context 'when 2 templates depend on packages with the same name' do
+    let(:jobs) {
+      [
         {
           'name' => 'foobar',
           'templates' => [
@@ -68,11 +109,32 @@ describe 'collocating templates from 2 releases' do
       ]
     }
 
+    it 'refuses to deploy' do
+      base_manifest = make_collocated_manifest
+      manifest_hash = base_manifest.merge(extras).merge('jobs' => jobs)
+
+      upload_stemcell_and_releases
+
+      deployment_manifest = yaml_file('simple', manifest_hash)
+      run_bosh("deployment #{deployment_manifest.path}")
+
+      output = run_bosh("deploy", failure_expected: true)
+      expect($?).not_to be_success
+      expect(output).to match(/Cannot tell which release to use for job `foobar'. Please reference an existing release./)
+    end
+  end
+
+  def make_collocated_manifest
     minimal_manifest = Bosh::Spec::Deployments.minimal_manifest
     minimal_manifest.delete('release')
-    manifest_hash = minimal_manifest.merge(extras)
+    minimal_manifest
+  end
 
-    output = deploy_simple_with_collocation(manifest_hash: manifest_hash, expect_failure: true)
-    output.should =~ /Cannot tell which release to use for job `foobar'. Please reference an existing release./
+  def upload_stemcell_and_releases
+    run_bosh("target http://localhost:#{current_sandbox.director_port}")
+    run_bosh('login admin admin')
+    run_bosh("upload release #{spec_asset('dummy-release.tgz')}")
+    run_bosh("upload release #{spec_asset('dummy2-release.tgz')}")
+    run_bosh("upload stemcell #{spec_asset('valid_stemcell.tgz')}")
   end
 end
