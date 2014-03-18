8c8dcff7e4056575df49b2de1ce4d7e7e9e23868
Kai Xiang <kxiang@pivotallabs.com>
2014-03-18 14:53:57 +0800

    Revert "exclude some test for warden-cpi"
    This reverts commit e22eaaf3b1f9b32d0e88e5376419f70d87709026.

--- bosh-stemcell/spec/os_image/ubuntu_spec.rb
+++ bosh-stemcell/spec/os_image/ubuntu_spec.rb
@@ -63,7 +63,7 @@ describe 'Ubuntu OS image' do
     end
   end
 
-  context 'installed by base_apt', exclude_on_warden: true do
+  context 'installed by base_apt' do
     %w(
       upstart
       build-essential
@@ -142,7 +142,7 @@ describe 'Ubuntu OS image' do
     end
   end
 
-  context 'installed by system_kernel', exclude_on_warden: true do
+  context 'installed by system_kernel' do
     %w(
       linux-image-virtual-lts-backport-oneiric
       linux-headers-virtual-lts-backport-oneiric
