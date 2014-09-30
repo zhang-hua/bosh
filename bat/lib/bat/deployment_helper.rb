14f26d9c38ed68679860569bb2866ee981bf8f18
Maria Shaldibina <mariash@pivotallabs.com>
2014-09-29 20:50:47 -0700

    On nebula n1.small is without extra disk

--- bat/lib/bat/deployment_helper.rb
+++ bat/lib/bat/deployment_helper.rb
@@ -153,7 +153,7 @@ module Bat
     end
 
     def use_flavor_with_no_ephemeral_disk
-      @spec['properties']['instance_type'] = 'no-ephemeral'
+      @spec['properties']['instance_type'] = 'n1.small'
     end
 
     def dynamic_network?
