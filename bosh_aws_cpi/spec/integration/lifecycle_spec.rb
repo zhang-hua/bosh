a0026b651b268f71791150758301390f884aaec9
Karl Isenberg <kisenberg@pivotal.io>
2014-11-04 09:31:55 -0800

    WIP tests to loop attaching disk after vm termination
    [#79810782]
    
    Signed-off-by: Phan Le <ple@pivotal.io>

--- bosh_aws_cpi/spec/integration/lifecycle_spec.rb
+++ bosh_aws_cpi/spec/integration/lifecycle_spec.rb
@@ -60,179 +60,85 @@ describe Bosh::AwsCloud::Cloud do
 
   extend Bosh::Cpi::CompatibilityHelpers
 
-  # Pass in *real* previously terminated instance id
-  # instead of just a made-up instance id
-  # because AWS returns Malformed error
-  # for instance ids that are not proper AWS hashed values.
-  it_can_delete_non_existent_vm 'i-49f9f169'
-
   describe 'ec2' do
-    let(:network_spec) do
-      {
-        'default' => {
-          'type' => 'dynamic',
-          'cloud_properties' => {}
+    describe 'deleting vms with persistent disk' do
+      let(:vip) { '192.168.0.0' } #TODO: get real vip
+      let(:network_spec) do
+        {
+          'static' => {
+            'type' => 'vip',
+            'ip' => vip,
+            'cloud_properties' => {}
+          }
         }
-      }
-    end
+      end
 
-    describe 'VM lifecycle with light stemcells' do
-      it 'exercises vm lifecycle with light stemcell' do
-        expect {
-          stemcell_id = cpi.create_stemcell('/not/a/real/path', { 'ami' => { 'us-east-1' => ami } })
-          instance_id = cpi.create_vm(
+      let(:options) do
+        {
+          user_known_hosts_file: %w[/dev/null],
+          keys: ['~/workspace/dolores/config/id_rsa_bosh'],
+          password: 'sudo-pass', #TODO: get password from ami!
+        }
+      end
+
+      def ssh(host, user, command, options = {})
+        options = options.dup
+        output = nil
+        put("--> ssh: #{user}@#{host} #{command.inspect}")
+        put("--> ssh options: #{options.inspect}")
+
+        Net::SSH.start(host, user, options) do |ssh|
+          output = ssh.exec!(command).to_s
+        end
+
+        put("--> ssh output: #{output.inspect}")
+        output
+      end
+
+      def ssh_sudo(host, user, command, options)
+        if options[:password].nil?
+          raise 'Need to set sudo :password'
+        end
+        ssh(host, user, "echo #{options[:password]} | sudo -p '' -S #{command}", options)
+      end
+
+      def mount_persistent_disk(instance_id)
+        settings = cpi.registry.read_settings(instance_id)
+        device_name = settings['disks']['persistent'].first
+        ssh_sudo(vip, 'ubuntu', "mkdir -p /tmp/persistent-disk && mount #{device_name} /tmp/persistent-disk", options)
+      end
+
+      describe '' do
+        @instance_id = cpi.create_vm(
+          'fake-agent-id',
+          ami,
+          { 'instance_type' => instance_type },
+          network_spec,
+          [],
+          {}
+        )
+        expect(@instance_id).not_to be_nil
+
+        @volume_id = cpi.create_disk(2048, {}, @instance_id)
+        expect(@volume_id).not_to be_nil
+
+        cpi.attach_disk(@instance_id, @volume_id)
+
+        mount_persistent_disk(@instance_id)
+
+        1.times do |i|
+          cpi.delete_vm(@instance_id)
+          @instance_id = cpi.create_vm(
             nil,
-            stemcell_id,
+            ami,
             { 'instance_type' => instance_type },
             network_spec,
             [],
             {}
           )
-          vm_metadata = { deployment: 'deployment', job: 'cpi_spec', index: '0', delete_me: 'please' }
-          cpi.set_vm_metadata(instance_id, vm_metadata)
-          cpi.delete_vm(instance_id)
-          cpi.delete_stemcell(stemcell_id)
-        }.not_to raise_error
-      end
-    end
-
-    context 'without existing disks' do
-      it 'should exercise the vm lifecycle' do
-        @instance_id = cpi.create_vm(
-          'agent-007',
-          ami,
-          { 'instance_type' => instance_type },
-          network_spec,
-          [],
-          { 'key' => 'value' }
-        )
-
-        expect(@instance_id).not_to be_nil
-
-        # possible race condition here
-        expect(cpi.has_vm?(@instance_id)).to be(true)
-
-        vm_metadata = { deployment: 'deployment', job: 'cpi_spec', index: '0', delete_me: 'please' }
-        cpi.set_vm_metadata(@instance_id, vm_metadata)
-
-        @volume_id = cpi.create_disk(2048, {}, @instance_id)
-        expect(@volume_id).not_to be_nil
-
-        cpi.attach_disk(@instance_id, @volume_id)
-
-        snapshot_metadata = vm_metadata.merge(
-          bosh_data: 'bosh data',
-          instance_id: 'instance',
-          agent_id: 'agent',
-          director_name: 'Director',
-          director_uuid: '6d06b0cc-2c08-43c5-95be-f1b2dd247e18',
-        )
-
-        snapshot_id = cpi.snapshot_disk(@volume_id, snapshot_metadata)
-        expect(snapshot_id).not_to be_nil
-
-        snapshot = cpi.ec2.snapshots[snapshot_id]
-        expect(snapshot.tags.device).to eq '/dev/sdf'
-        expect(snapshot.tags.agent_id).to eq 'agent'
-        expect(snapshot.tags.instance_id).to eq 'instance'
-        expect(snapshot.tags.director_name).to eq 'Director'
-        expect(snapshot.tags.director_uuid).to eq '6d06b0cc-2c08-43c5-95be-f1b2dd247e18'
-
-        expect(snapshot.tags[:Name]).to eq 'deployment/cpi_spec/0/sdf'
-
-        cpi.delete_snapshot(snapshot_id)
-
-        Bosh::Common.retryable(tries: 20, on: Bosh::Clouds::DiskNotAttached, sleep: lambda { |n, _| [2**(n-1), 30].min }) do
-          cpi.detach_disk(@instance_id, @volume_id)
-          true
+          cpi.attach_disk(@instance_id, @volume_id)
+          mount_persistent_disk(@instance_id)
         end
-      end
-    end
-
-    context 'with existing disks' do
-      before { @existing_volume_id = cpi.create_disk(2048, {}) }
-      after  { cpi.delete_disk(@existing_volume_id) if @existing_volume_id }
-
-      it 'should exercise the vm lifecycle' do
-        @instance_id = cpi.create_vm(
-          'agent-007',
-          ami,
-          { 'instance_type' => instance_type },
-          network_spec,
-          [@existing_volume_id],
-          { 'key' => 'value' }
-        )
-
-        expect(@instance_id).not_to be_nil
-
-        # possible race condition here
-        expect(cpi.has_vm?(@instance_id)).to be(true)
-
-        metadata = { deployment: 'deployment', job: 'cpi_spec', index: '0', delete_me: 'please' }
-        cpi.set_vm_metadata(@instance_id, metadata)
-      end
-
-      it 'should list the disks' do
-        @instance_id = cpi.create_vm(
-          'agent-007',
-          ami,
-          { 'instance_type' => instance_type },
-          network_spec,
-          [@existing_volume_id],
-          { 'key' => 'value' }
-        )
-
-        expect(@instance_id).not_to be_nil
-
-        # possible race condition here
-        expect(cpi.has_vm?(@instance_id)).to be(true)
-
-        metadata = { deployment: 'deployment', job: 'cpi_spec', index: '0', delete_me: 'please' }
-        cpi.set_vm_metadata(@instance_id, metadata)
-
-        @volume_id = cpi.create_disk(2048, {}, @instance_id)
-        expect(@volume_id).not_to be_nil
-
-        cpi.attach_disk(@instance_id, @volume_id)
-        expect(cpi.get_disks(@instance_id)).to include(@volume_id)
-
-        Bosh::Common.retryable(tries: 20, on: Bosh::Clouds::DiskNotAttached, sleep: lambda { |n, _| [2**(n-1), 30].min }) do
-          cpi.detach_disk(@instance_id, @volume_id)
-          true
-        end
-      end
-    end
-  end
-
-  describe 'vpc' do
-    let(:network_spec) do
-      {
-        'default' => {
-          'type' => 'manual',
-          'ip' => @manual_ip, # use different IP to avoid race condition
-          'cloud_properties' => { 'subnet' => @subnet_id }
-        }
-      }
-    end
-
-    context 'without existing disks' do
-      it 'should exercise the vm lifecycle' do
-        @instance_id = cpi.create_vm(
-          'agent-007',
-          ami,
-          { 'instance_type' => instance_type },
-          network_spec,
-          [],
-          { 'key' => 'value' }
-        )
-
-        expect(@instance_id).not_to be_nil
-
-        # possible race condition here
-        expect(cpi.has_vm?(@instance_id)).to be(true)
-
-        metadata = { deployment: 'deployment', job: 'cpi_spec', index: '0', delete_me: 'please' }
-        cpi.set_vm_metadata(@instance_id, metadata)
       end
     end
   end
